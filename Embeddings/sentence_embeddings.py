7239ba08b80aef385bb667deff58b86f6774cfc0
Aditya Gsr <gsraditya2909@gmail.com>
2026-01-10 11:54:59 +0530

    sentence embeddings

--- Embeddings/sentence_embeddings.py
+++ Embeddings/sentence_embeddings.py
@@ -5,7 +5,7 @@ def sentence_embeddings():
     # It's based on BERT architecture and fine-tuned for generating sentence embeddings
     model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
     # 'paraphrase-MiniLM-L6-v2' is the name of the pre-trained model being used
-    sentences = ["This is an example sentence", "Each sentence is converted to a vector"]
+    sentences = ["This is an example sentence"]
     embeddings = model.encode(sentences)
     print("Sentence Embedding shape:", embeddings.shape)
     print("First sentence embedding:", embeddings[0][:5])  # First 5 dimensions
